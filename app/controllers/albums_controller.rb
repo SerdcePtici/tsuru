1c27fe3bd64eda7480ebca2cd1016467bac93f28
Oleg Grigoriev <mehanoid.ru@gmail.com>
2014-03-28 21:48:22 +0400

    Ajax form for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,9 +2,24 @@ class AlbumsController < InheritedResources::Base
   load_and_authorize_resource
   belongs_to :topic, shallow: true
   actions :all, except: :index
+  respond_to :html, :js
 
   before_action do
     @current_menu_item = :topic
+  end
+
+  def create
+    create! do |success, failure|
+      failure.js { render action: 'new' }
+    end
+  end
+
+  def upload_pictures
+    @lesson = Album.new permitted_params[:album]
+    @lesson.upload!
+    respond_to do |format|
+      format.js { render action: 'new' }
+    end
   end
 
   private
