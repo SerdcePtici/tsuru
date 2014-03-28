2d7d118b2c2e412a200a345b3d60b06ff3afc5c2
Oleg Grigoriev <mehanoid.ru@gmail.com>
2014-03-29 03:00:03 +0400

    Fix comment form replacement

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -4,10 +4,10 @@ module CommentsHelper
   end
 
   def new_comment
-    if resource.kind_of?(Comment)
-      resource.new_record? ? resource : parent.comments.build
+    if resource.kind_of?(Comment) && resource.new_record?
+       resource
     else
-      resource.comments.build
+      commentable.comments.build
     end
   end
 end
